080e7f3f41b87e722a5746a3cdeb2c4ad184f9c3
Igor Goldny <igoldny@gmail.com>
2013-02-17 18:32:36 +0200

    kick starter

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ftbpro::Application.config.secret_token = '647f50124f629b7cd6bc838e7fd3349b85931f7d07a804759cb9631c08f370c1c7ee9ac7f821d60ff9b012d0bfbf59f0472ecaf4f13ba280b24d3a5ce105b708'
